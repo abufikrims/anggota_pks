b525a989ddeef308340b4e96d308e8a5f0c87886
Imam Masyhuri <abufikri.ms@gmail.com>
2021-04-19 13:31:14 +0700

    Tambahkan Foto dan KTP

--- model/pks_anggota.py
+++ model/pks_anggota.py
@@ -27,7 +27,7 @@ class pks_anggota(models.Model):
 
 
     #Jika Alamat Tinggal beda dengan KTP
-    tg_sesuai_ktp       = fields.Selection(string='Tempat Tinggal', selection=[('1', 'Sesuai KTP'), ('0', 'Beda Lokasi Tinggal'),], required=True)
+    tg_sesuai_ktp       = fields.Selection(string='Tempat Tinggal', selection=[('1', 'Sesuai KTP'), ('0', 'Beda Lokasi Tinggal'),], required=True, default="0")
     
     tg_street           = fields.Char(string='Nama Jalan')
     tg_street2          = fields.Char(string='RT/RW')
@@ -64,13 +64,13 @@ class pks_anggota(models.Model):
     keluarga_ids        = fields.One2many(comodel_name="pks_keluarga",  inverse_name="anggota_id",  string="Keluarga", ondelete='cascade', help="")
 
 
-    spouse_nama         = fields.Char('Nama Suami/Istri')
-    spouse_jns_kelamin  = fields.Selection(selection=[('laki-laki','Laki-Laki'),('perempuan','Perempuan')],  string="Jns kelamin", help="")
-    spouse_tmp_lahir    = fields.Char( string="Tmp lahir",  help="")
-    spouse_tgl_lahir   = fields.Date( string="Tgl lahir",  help="")
-    spouse_mobile       = fields.Char(string='Nomor HP')
-    spouse_pekerjaan_id = fields.Many2one(comodel_name="ref_pekerjaan",  string="Pekerjaan",  help="")
-    spouse_pendidikan_id= fields.Many2one(comodel_name="ref_pendidikan",  string="Pendidikan",  help="")
+    spouse_nama         = fields.Many2one(comodel_name='pks_anggota', string='Nama Suami/Istri')
+    spouse_jns_kelamin  = fields.Selection(selection=[('laki-laki','Laki-Laki'),('perempuan','Perempuan')], related="spouse_nama.jns_kelamin", string="Jns kelamin", help="")
+    spouse_tmp_lahir    = fields.Char( string="Tmp lahir", related="spouse_nama.tmp_lahir",  help="")
+    spouse_tgl_lahir   = fields.Date( string="Tgl lahir", related="spouse_nama.tgl_lahir",  help="")
+    spouse_mobile       = fields.Char(string='Nomor HP', related="spouse_nama.mobile", help="")
+    spouse_pekerjaan_id = fields.Many2one(comodel_name="ref_pekerjaan",  string="Pekerjaan", related="spouse_nama.pekerjaan_id",  help="")
+    spouse_pendidikan_id= fields.Many2one(comodel_name="ref_pendidikan",  string="Pendidikan", related="spouse_nama.pendidikan_id",  help="")
 
     spouse_last_jenjang = fields.Selection(string='Jenjang Tarbiyah', selection=[('pendukung', 'Pendukung'), ('penggerak', 'Penggerak'),('pelopor', 'Pelopor'),])
     sp_tarbiyah1a_date  = fields.Date(string='Jenjang Pemula')
@@ -80,6 +80,10 @@ class pks_anggota(models.Model):
     sp_tarbiyah3a_date  = fields.Date(string='Jenjang Madya')
     sp_tarbiyah3b_date  = fields.Date(string='Jenjang Dewasa')
     sp_tarbiyah3c_date  = fields.Date(string='Jenjang Utama')
+
+    # Dokumen KTP
+    dokumen_ktp         = fields.Binary(string='Dokumen KTP')
+    dokumen_name        = fields.Char(string="Nama File")
     
     @api.onchange('kota_id')
     def _onchange_kota_id(self):
