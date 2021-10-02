b86c7c8d25fc6e31bd423ce387753c84555d5996
Abu Fikri <abufikrims@gmail.com>
2021-10-02 21:27:17 +0700

    revisi rekruitmen

--- model/rekruitmen.py
+++ model/rekruitmen.py
@@ -9,13 +9,19 @@ class rekruitmen(models.Model):
     name            = fields.Char(string="No. Referensi",  help="", readonly=True, default='Auto')
     tanggal         = fields.Date(string='Tanggal', required=True, default=fields.Date.context_today)
     #rekruiter       = fields.Many2one(comodel_name='pks_anggota', required=True, string='Direkruit Oleh')
-    jns_rekruitmen  = fields.Selection(string='Jenis Rekrutmen', selection=[('upa', 'UPA'), ('bidang', 'Bidang'),('dpc','DPC')], required=True)
+    jns_rekruitmen  = fields.Selection(string='Jenis Rekrutmen', selection=[('upa', 'UPA'), ('bidang', 'Bidang'),('dpc','DPC')], default='upa')
     anggota_ids     = fields.One2many(comodel_name='pks_anggota', inverse_name='rekrutmen_id', string='Daftar Anggota Rekrutmen')
     bidang_id       = fields.Many2one(comodel_name='bidang_pks', string='Nama Bidang', domain=[('is_recruiter','=',True)])
     upa_id          = fields.Many2one(comodel_name='kelas_tarbiyah', string='Nama UPA')
     dpc_id          = fields.Many2one(comodel_name='struktural_pks', string='Nama DPC')
-    
-    
+    keterangan      = fields.Char(string='Keterangan')
+
+    @api.onchange('jns_rekruitmen')
+    def _onchange_(self):
+        if self.jns_rekruitmen == 'upa':
+            rec = self.env['pks_anggota'].sudo().search([('partner_id.id','=',self.env.user.partner_id.id)])
+            if rec:
+                self.upa_id     = rec[0].halaqoh_id.id
 
     @api.model
     def create(self, vals):
